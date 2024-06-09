7cfb9aa9e4fb825e265ad5068570704302d884dc
Shruti Kannan <shruti.kannan1@gmail.com>
2024-06-09 19:15:42 +0530

    Update bubble_sort.c
    Typo

--- Arrays/bubble_sort.c
+++ Arrays/bubble_sort.c
@@ -10,7 +10,7 @@ void print_array(int* arr, int n) {
 
 void sort_array_des(int* arr, int n) {
     int temp;
-    // Moving smaller elements to the front
+    // Moving smaller elements to the end
     for (int i = 0; i < n; i++) {
         for (int j = i; j < n; j++) {
             if (arr[i] < arr[j]) {
